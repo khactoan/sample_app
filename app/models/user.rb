cffd9c499c744b7fa94e511ac48cc6192995d138
ToanDK <khactoan96@gmail.com>
2017-10-19 13:31:27 +0700

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,30 @@ class User < ApplicationRecord
   validates :password, presence: true,
     length: {minimum: Settings.min_length_password}
 
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
   class << self
     def digest string
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
         BCrypt::Engine.cost
       BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
     end
   end
 end
